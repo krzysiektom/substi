96ec1e206a456acd2233f13405f761a9ff1204b6
krzysztof <krzysiektom@op.pl>
2019-09-20 14:03:41 +0200

    Initial commit

---
+++ src/main/java/com/example/substi/SubstiApplication.java
@@ -0,0 +1,13 @@
+package com.example.substi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubstiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SubstiApplication.class, args);
+    }
+
+}
