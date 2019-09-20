33d4428496f3dcda21038f6b96cf53fd3d99992a
krzysztof <krzysiektom@op.pl>
2019-09-20 15:25:29 +0200

    add thymeleaf

--- src/main/java/com/example/substi/HomeController.java
+++ src/main/java/com/example/substi/HomeController.java
@@ -1,15 +1,20 @@
 package com.example.substi;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 @Controller
 public class HomeController {
 
     @RequestMapping("/")
-    @ResponseBody
-    public String home(){
-        return "Home page";
+    public String home(Model model){
+        DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, Locale.getDefault());
+        model.addAttribute("serverTime", dateFormat.format(new Date()));
+        return "home";
     }
 }
